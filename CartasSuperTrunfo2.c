43f12418af9d587e106a8d33b4424970ab349f09
Thiago11000 <garciathiagodejesus86@gmail.com>
2025-04-03 23:12:09 +0000

    corrigindo

---
+++ CartasSuperTrunfo2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {  
+
+    int populacao1, populacao2, Numerodepontosturisticos1, Numerodepontosturisticos2;
+    float area1, PIB1, area2, PIB2;
+    char estado1, estado2;
+    char Codigodacarta1[50];
+    char Codigodacarta2[50];
+    char Nomedacidade1[50];
+    char Nomedacidade2[50];
+
+    printf("Digite o Estado: \n");
+    scanf("%c", &estado1);
+
+    printf("Digite o Codigo: \n");
+    scanf("%s", &Codigodacarta1);
+
+    printf("Digite o Nome da Cidade: \n");
+    scanf("%s", &Nomedacidade1);
+
+    printf("Digite a População: \n");
+    scanf("%d", &populacao1);
+
+    printf("Digite a Área: \n");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB1);
+
+    printf("Digite o Número de pontos Turisticos: \n");
+    scanf("%d", &Numerodepontosturisticos1);
+
+    printf("Carta 1: \n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", Codigodacarta1);
+    printf("Nome da Cidade: %s\n", Nomedacidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %f\n", area1);
+    printf("PIB: %f\n", PIB1);
+    printf("Número de Pontos Turísticos: %d\n", Numerodepontosturisticos1);
+
+    return 0;
+
+}
