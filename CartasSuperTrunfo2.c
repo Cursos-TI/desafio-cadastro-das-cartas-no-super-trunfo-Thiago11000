29517947efd4b8900517b7aa0625af66df51c252
Thiago11000 <garciathiagodejesus86@gmail.com>
2025-04-04 13:09:06 +0000

    corrigindo supertrunfo

--- CartasSuperTrunfo2.c
+++ CartasSuperTrunfo2.c
@@ -14,10 +14,10 @@ int main() {
     scanf("%c", &estado1);
 
     printf("Digite o Codigo: \n");
-    scanf("%s", &Codigodacarta1);
+    scanf("%s", Codigodacarta1);
 
     printf("Digite o Nome da Cidade: \n");
-    scanf("%s", &Nomedacidade1);
+    scanf("%s", Nomedacidade1);
 
     printf("Digite a População: \n");
     scanf("%d", &populacao1);
@@ -29,7 +29,7 @@ int main() {
     scanf("%f", &PIB1);
 
     printf("Digite o Número de pontos Turisticos: \n");
-    scanf("%d\n", &Numerodepontosturisticos1);
+    scanf("%d", &Numerodepontosturisticos1);
 
     printf("Carta 1: \n");
     printf("Estado: %c\n", estado1);
@@ -40,6 +40,36 @@ int main() {
     printf("PIB: %f\n", PIB1);
     printf("Número de Pontos Turísticos: %d\n", Numerodepontosturisticos1);
 
+    printf("Digite o Estado: \n");
+    scanf("%c", &estado2);
+
+    printf("Digite o Codigo: \n");
+    scanf("%s", Codigodacarta2);
+
+    printf("Digite o Nome da Cidade: \n");
+    scanf("%s", Nomedacidade2);
+
+    printf("Digite a População: \n");
+    scanf("%d", &populacao2);
+
+    printf("Digite a Área: \n");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB2);
+
+    printf("Digite o Número de pontos Turisticos: \n");
+    scanf("%d", &Numerodepontosturisticos2);
+
+
+    printf("Carta 2: \n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", Codigodacarta2);
+    printf("Nome da Cidade: %s\n", Nomedacidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %f\n", area2);
+    printf("PIB: %f\n", PIB2);
+    printf("Número de Pontos Turísticos: %d\n", Numerodepontosturisticos2);
     return 0;
 
 }
